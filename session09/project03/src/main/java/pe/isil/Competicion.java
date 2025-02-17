2521c07e5ea5417c470ec773214d9b1a5ed14e80
jmayta1984 <jmayta@pucp.edu.pe>
2025-02-17 08:10:50 -0500

    feat: add methods pruebaPartcipacion & mejorMarcaPersonal

--- session09/project03/src/main/java/pe/isil/Competicion.java
+++ session09/project03/src/main/java/pe/isil/Competicion.java
@@ -2,6 +2,8 @@ package pe.isil;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
 
 public class Competicion {
     private ArrayList<Participante> participantes;
@@ -18,9 +20,29 @@ public class Competicion {
         participantes.add(participante);
     }
 
-    public void ordenParticipacion(){
-        Collections.sort(participantes , new ComparadorParticipante());
+    public void ordenParticipacion() {
+        Collections.sort(participantes, new ComparadorParticipante());
     }
-    
-    
+
+    public void pruebaParticipacion() {
+        Scanner scanner = new Scanner(System.in);
+
+        for (Participante participante : participantes) {
+            System.out.println("Ingrese la marca obtenida para el participante " + participante.getNombre() + ":");
+            participante.setMarcaPrueba(scanner.nextDouble());
+        }
+        scanner.close();
+    }
+
+    public List<Participante> mejorMarcaPersonal(){
+        List<Participante> mejorMarca = new ArrayList<>();
+
+        for (Participante participante : participantes) {
+            if (participante.getMarcaPersonal() > participante.getMarcaPrueba()) {
+                mejorMarca.add(participante);
+            }
+        }
+        return mejorMarca;
+    }
+
 }
